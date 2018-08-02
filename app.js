6fb3da47af82bbe07f14aabd4f2acfe78c96956b
Tom DeVito <tomdevito27@gmail.com>
2018-08-02 12:52:19 -0400

    file structure

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const express = require('express');
 const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('hello tweety!');
+})
+
+app.listen(PORT);
